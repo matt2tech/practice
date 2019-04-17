e55d5d14cb75cbfbf21f27c5b332665bb6f8d0f8
matt2tech <mlipsey@basecampcodingacademy.org>
2019-04-17 10:20:31 -0500

    Genesis 1:1

---
+++ 03_Wed/web/javascriptTwo.js
@@ -0,0 +1,20 @@
+document.getElementById("btn").addEventListener("click", function() {
+    var one = document.getElementById("stringOne").value;
+    var two = document.getElementById("stringTwo").value;
+
+    document.getElementById("resultTwo").innerText = string;
+    var num = Math.min(one.length, two.length);
+    var string = "";
+    var x = 0
+
+    while(x < num) {
+        if (one[x].toLowerCase() == two[x].toLowerCase()) {
+            string += one[x];
+        } else {
+            string += ".";
+        }
+        x++
+    }
+    string += ".".repeat((Math.max(one.length, two.length) - num));
+    document.getElementById("resultTwo").innerText = string;
+})
